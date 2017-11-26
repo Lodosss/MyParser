498fad308acd5de5741fde7c6aa21b349db4c170
Opossumm <30445605+Opossumm@users.noreply.github.com>
2017-11-26 22:25:54 +0200

    second commit

--- Parser.py
+++ Parser.py
@@ -13,3 +13,20 @@ def pars(str, sub=" "):
     if buf != "":
         result.append(buf)
     return result
+
+def parstxt(file, sub = " "):
+    str = file.read()
+    result = []
+    buf = ""
+    for i in range(0, len(str)):
+        if str[i] != sub:
+            buf = buf + str[i]
+
+        if str[i] == sub:
+            if buf != "":
+                result.append(buf)
+                buf = ""
+
+    if buf != "":
+        result.append(buf)
+    return result
