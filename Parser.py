63ba097c8aa4da1ad62d64e4f855d2527006fa6f
Opossumm <30445605+Opossumm@users.noreply.github.com>
2017-11-26 22:12:22 +0200

    First comit

---
+++ Parser.py
@@ -0,0 +1,15 @@
+def pars(str, sub=" "):
+    result = []
+    buf=""
+    for i in range(0, len(str)):
+        if str[i] != sub:
+            buf = buf+str[i]
+
+        if str[i] == sub:
+            if buf != "":
+               result.append(buf)
+               buf = ""
+
+    if buf != "":
+        result.append(buf)
+    return result
